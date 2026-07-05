a8d842e70123eb3e8ffd0b2173bad4f62f0a2ebf
Arjun Ingole <technoarjun@gmail.com>
2026-07-05 23:59:35 +0530

    added sidebar error

--- client/src/pages/AiStudio/AIStudio.jsx
+++ client/src/pages/AiStudio/AIStudio.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./AIStyles/AIStudio.css";
+import "./styles/ai/AIStudio.css";
 
 import ProductSelector from "./components/ai/ProductSelector";
 
