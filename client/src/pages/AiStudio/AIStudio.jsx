9270274e5b8a356bbbe6382a053bb1a6ef0e61bb
Arjun Ingole <technoarjun@gmail.com>
2026-07-06 00:08:02 +0530

    added

--- client/src/pages/AiStudio/AIStudio.jsx
+++ client/src/pages/AiStudio/AIStudio.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./styles/ai/AIStudio.css";
+import "../AiStudio/AiStyles/AiStudio.css";
 
 import ProductSelector from "./components/ai/ProductSelector";
 
