d69901d700b4d101b34443041de3bf97b7f6ef5b
Arjun Ingole <technoarjun@gmail.com>
2026-07-07 22:02:58 +0530

    Fix cloudinary.js rev

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -12,32 +12,32 @@ const apiKey = decodeURIComponent(url.username);
 const apiSecret = decodeURIComponent(url.password);
 const cloudName = url.hostname;
 
-// cloudinary.config({
-//   cloud_name: cloudName,
-//   api_key: apiKey,
-//   api_secret: apiSecret,
-//   secure: true,
-// });
-
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecret,
   secure: true,
 });
 
-const config = cloudinary.config();
-
-// console.log("Cloudinary SDK config:", {
-//   cloud_name: !!config.cloud_name,
-//   api_key: !!config.api_key,
-//   api_secret: !!config.api_secret,
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+//   secure: true,
 // });
 
+const config = cloudinary.config();
+
 console.log("Cloudinary SDK config:", {
-  cloud_name: !!process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: !!process.env.CLOUDINARY_API_KEY,
-  api_secret: !!process.env.CLOUDINARY_API_SECRET,
+  cloud_name: !!config.cloud_name,
+  api_key: !!config.api_key,
+  api_secret: !!config.api_secret,
 });
+
+// console.log("Cloudinary SDK config:", {
+//   cloud_name: !!process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: !!process.env.CLOUDINARY_API_KEY,
+//   api_secret: !!process.env.CLOUDINARY_API_SECRET,
+// });
 
 module.exports = cloudinary;
